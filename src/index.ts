8916ceaabc356529de8c387eff9fb7c015f08d52
Ellis Pritchard <ellispritchard@me.com>
2023-01-15 13:34:09 +0000

    basic express/react dev app setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    res.send("index.html");
+  } catch (error) {
+    next(error);
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
+
